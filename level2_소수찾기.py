ad6ead3a4ddc90ba04d8aa7515e1f173bbec5a86
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-06-28 11:29:09 +0900

    Update level2_소수찾기.py

--- level2_소수찾기.py
+++ level2_소수찾기.py
@@ -1,13 +1,13 @@
 from itertools import permutations
+from math import sqrt
 def solution(numbers):
     sosu = set()
     for i in range(1,len(numbers)+1):
         for c in permutations(list(numbers), i):
             num = int("".join(list(c)))
-            #3. 소수 판별
             if num == 1 or num == 0: continue
             check = True
-            for n in range(2,num//2+1):
+            for n in range(2,int(sqrt(num))+1):
                 if num % n == 0:
                     check = False
                     break
