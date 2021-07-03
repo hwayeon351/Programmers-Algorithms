1424c34b58226d3c7de7fb6b0dc4c36c31568ea7
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-03 18:27:25 +0900

    Update level3_단어변환.py

--- level3_단어변환.py
+++ level3_단어변환.py
@@ -1,9 +1,9 @@
 def dfs(i, target, words, visit, cnt):
     global answer
     if words[i] == target:
-        if answer > cnt: answer = cnt
+        if answer == 0 or answer > cnt: answer = cnt
         return
-    if cnt == len(visit): return
+    if cnt == len(visit)-1: return
     for b_i in range(len(words[i])):
         for idx, w in enumerate(words):
             if words[i][:b_i]==w[:b_i] and words[i][b_i+1:]==w[b_i+1:] and visit[idx]==False:
@@ -14,12 +14,10 @@ def dfs(i, target, words, visit, cnt):
 def solution(begin, target, words):
     global answer
     if target not in words: return 0
-    answer = len(words)
+    answer = 0
+    words.append(begin)
     visit = [False]*len(words)
-    for b_i in range(len(begin)):
-        for i, w in enumerate(words):
-            if w[:b_i]==begin[:b_i] and w[b_i+1:]==begin[b_i+1:]:
-                visit[i] = True 
-                dfs(i, target, words, visit, 1)
-                visit[i] = False
+    visit[-1] = True
+    dfs(-1, target, words, visit, 0)
+    
     return answer
