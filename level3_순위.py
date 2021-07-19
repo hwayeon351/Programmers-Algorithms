4ee431e2f2e340e60e135f22dc150ee09f2e60a8
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-19 18:40:13 +0900

    Update level3_순위.py

--- level3_순위.py
+++ level3_순위.py
@@ -1,19 +1,20 @@
 from collections import defaultdict
 def solution(n, results):
     answer = 0
-    win_d = defaultdict(set)
-    lose_d = defaultdict(set)
-    for winner, loser in results:
-        win_d[winner].add(loser)
-        lose_d[loser].add(winner)
+    #key = 사람, value = key를 이긴 사람
+    winner = defaultdict(set)
+    #key = 사람, value = key한테 진 사람
+    loser = defaultdict(set)
+    for win, lose in results:
+        winner[lose].add(win)
+        loser[win].add(lose)
     for i in range(1, n+1):
-        #i를 이긴 사람은 i한테 진 사람도 이긴다.
-        for winner in lose_d[i]:
-            win_d[winner].update(win_d[i])
-        #i한테 진 사람은 i를 이긴 사람한테도 진다.
-        for loser in win_d[i]:
-            lose_d[loser].update(lose_d[i])
+        #i를 이긴 사람들은 i한테 진 사람들도 이긴다
+        for w in winner[i]:
+            loser[w].update(loser[i])
+        #i한테 진 사람들은 i를 이긴 사람들한테도 진다
+        for l in loser[i]:
+            winner[l].update(winner[i])
     for i in range(1, n+1):
-        if len(win_d[i])+len(lose_d[i]) == n-1:
-            answer += 1
+        if len(winner[i]) + len(loser[i]) == n-1: answer += 1
     return answer
