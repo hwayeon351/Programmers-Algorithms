3becb2c3eea3f339dd560693aaa48ad966d0309f
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-30 19:49:07 +0900

    Update level1_[1차]다트게임.py

--- level1_[1차]다트게임.py
+++ level1_[1차]다트게임.py
@@ -1,32 +1,42 @@
+def cal_score(s, b, op, score):
+    s **= b
+    if op == "*":
+        if len(score) > 0:
+            score[-1] *= 2
+        score.append(s*2)
+    elif op == "#":
+        score.append(-s)
+    else: score.append(s)
+    
 def solution(dartResult):
-    bonus = ['S', 'D' ,'T']
-    op = ['*', '#']
-    cnt = -1
-    arr = []
-    for i in range(len(dartResult)):
-        if dartResult[i] in bonus:
-            if dartResult[i] == 'D':
-                arr[cnt] = arr[cnt] ** 2
-            elif dartResult[i] == 'T':
-                arr[cnt] = arr[cnt] ** 3
-
-        elif dartResult[i] in op:
-            if cnt == 0:
-                if dartResult[i] == '*':
-                    arr[cnt] = arr[cnt] * 2
-                if dartResult[i] == '#':
-                    arr[cnt] = arr[cnt] * -1
-            else:
-                if dartResult[i] == '*':
-                    arr[cnt] = arr[cnt] * 2
-                    arr[cnt-1] = arr[cnt-1] * 2
-                elif dartResult[i] == '#':
-                    arr[cnt] = arr[cnt] * -1
-        else:
-            if dartResult[i] == '0':
-                if dartResult[i-1].isdigit():
-                    arr[cnt] = int(dartResult[i-1]+dartResult[i])
-                    continue
-            cnt += 1
-            arr.append(int(dartResult[i]))
-    return sum(arr)
+    score = []
+    option = ['*','#']
+    s = 0
+    b = 0
+    op = ""
+    i = 0
+    while i < len(dartResult):
+        if dartResult[i].isdigit():
+            if i != 0:
+                cal_score(s, b, op, score)
+                s = 0
+                b = 0
+                op = ""
+            if dartResult[i] == '1':
+                if dartResult[i+1] == '0':
+                    s = 10
+                    i += 1
+                else: s = 1
+            else: s = int(dartResult[i])
+        elif dartResult[i] == 'S':
+            b = 1
+        elif dartResult[i] == 'D':
+            b = 2
+        elif dartResult[i] == 'T':
+            b = 3
+        elif dartResult[i] in option:
+            op = dartResult[i]
+        if i == len(dartResult)-1:
+            cal_score(s, b, op, score)
+        i += 1
+    return sum(score)
