781f9026918317f13e94dd09420f508b40f3331a
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-30 20:06:32 +0900

    Update level2_[1차]캐시.py

--- level2_[1차]캐시.py
+++ level2_[1차]캐시.py
@@ -1,18 +1,16 @@
+from collections import deque
 def solution(cacheSize, cities):
+    if cacheSize == 0: return 5*len(cities)
     answer = 0
-    cache = []
-    if cacheSize == 0: return len(cities)*5
+    cache = deque()
     for city in cities:
         city = city.lower()
-        if city in cache:
-            answer += 1
-            cache.pop(cache.index(city))
+        if city not in cache:
+            if len(cache) == cacheSize: cache.popleft()
             cache.append(city)
-        else:
             answer += 5
-            if len(cache) == cacheSize:
-                cache.pop(0)
-                cache.append(city)
-            else:
-                cache.append(city)
+        else:
+            del cache[cache.index(city)]
+            cache.append(city)
+            answer += 1
     return answer
