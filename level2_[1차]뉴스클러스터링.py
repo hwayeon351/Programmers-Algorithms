098af084805026e10a720a4fdc71eaf795cc9323
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-30 18:34:27 +0900

    Update level2_[1차]뉴스클러스터링.py

--- level2_[1차]뉴스클러스터링.py
+++ level2_[1차]뉴스클러스터링.py
@@ -1,27 +1,26 @@
+from collections import Counter
 def solution(str1, str2):
     str1 = str1.lower()
     str2 = str2.lower()
-    arr1 = []
-    arr2 = []
-    inter = 0
-    for i in range(len(str1)-1):
-        if (str1[i]+str1[i+1]).isalpha():
-            arr1.append(str1[i]+str1[i+1])
-    for i in range(len(str2)-1):
-        if (str2[i]+str2[i+1]).isalpha():
-            arr2.append(str2[i]+str2[i+1])
-    if len(arr1) == 0 and len(arr2) == 0: return 65536
-    arr1.sort()
-    arr2.sort()
-    last = ""
-    for a in arr1:
-        if a == last: continue
-        cnt = arr2.count(a)
-        if cnt > 1:
-            inter += min(cnt, arr1.count(a))
-            last = a
-            continue
-        inter += cnt
-        last = a
-
-    return int(inter / (len(arr2) - inter + len(arr1)) * 65536)
+    A = []
+    B = []
+    i = 0
+    #다중 집합 만들기
+    while i <= max(len(str1)-2, len(str2)-2):
+        if i <= len(str1)-2:
+            if str1[i].isalpha() and str1[i+1].isalpha(): A.append(str1[i] + str1[i+1])
+        if i <= len(str2)-2:
+            if str2[i].isalpha() and str2[i+1].isalpha(): B.append(str2[i] + str2[i+1])
+        i+=1
+        
+    #A와 B가 공집합인 경우
+    if len(A) == len(B) == 0: return 65536
+    
+    #유사도 계산
+    counterA = Counter(A)
+    counterB = Counter(B)
+    inter_AB = 0
+    for k in counterA.keys():
+        inter_AB += min(counterA[k], counterB[k])
+    sum_AB = sum(counterA.values()) + sum(counterB.values()) - inter_AB
+    return int(inter_AB/sum_AB*65536)
