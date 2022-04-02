aa326a5d2d8fad682716a5711af7865ef6bd4044
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-02 17:23:38 +0900

    Update level3_기둥과보설치.py

--- level3_기둥과보설치.py
+++ level3_기둥과보설치.py
@@ -1,15 +1,15 @@
-def impossible(answer):
+def check_possible(answer):
     for x, y, a in answer:
         #기둥
         if a == 0:
             if (y == 0) or ([x, y, 1] in answer) or ([x-1, y, 1] in answer) or ([x, y-1, 0] in answer): continue
-            return True
+            return False
         #보
         else:
             if ([x, y-1, 0] in answer) or ([x+1, y-1, 0] in answer) or ([x-1, y, 1] in answer and [x+1, y, 1] in answer): continue
-            return True
+            return False
         
-    return False
+    return True
     
 def solution(n, build_frame):
     answer = []
@@ -18,12 +18,12 @@ def solution(n, build_frame):
         #설치
         if b == 1:
             answer.append(item)
-            if impossible(answer):
+            if not check_possible(answer):
                 answer.remove(item)
         #삭제
         elif item in answer:
             answer.remove(item)
-            if impossible(answer):
+            if not check_possible(answer):
                 answer.append(item)
                 
     return sorted(answer, key = lambda x:(x[0], x[1], x[2]))
