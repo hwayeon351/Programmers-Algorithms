0b806fdda5a947ba271546e427cfe1f2f0856aaa
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-05-06 17:30:25 +0900

    Update level3_블록이동하기.py

--- level3_블록이동하기.py
+++ level3_블록이동하기.py
@@ -1,87 +1,71 @@
-#8, 12, 14 테케 통과 못함
-from collections import defaultdict, deque
+from collections import deque
 
-rotate = [[1, 0, 1, 1], [-1, 0, -1, 1], [1, 0, 1, -1], [-1, 0, -1, -1], [0, 1, 1, 1], [0, 1, -1, 1], [0, -1, 1, -1],
-          [0, -1, -1, -1]]
-move = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-
-def play(board, N):
-    visit = defaultdict(int)
-    visit['0010'] = 1
-    q = deque([[[[0, 0], [1, 0]], 0]])
+def bfs(R, C):
+    global brd, rotate
+    # (r1, c1), (r2, c2), d
+    # d = 0 -> 가로, d = 1 -> 세로
+    visit = [[[[[0] * len(brd[0]) for _ in range(len(brd))] for _ in range(len(brd[0]))] for _ in
+               range(len(brd))] for _ in range(2)]
+    dr = [-1, 0, 1, 0]
+    dc = [0, -1, 0, 1]
+    robot = [[0, 0], [0, 1], 0]
+    visit[robot[2]][robot[0][0]][robot[0][1]][robot[1][0]][robot[1][1]] = 1
+    q = deque([[robot, 0]])
 
     while q:
-        r, sec = q.popleft()
-        if (r[0][0] == N and r[0][1] == N) or (r[1][0] == N and r[1][1] == N): return sec
+        rb, sec = q.popleft()
+        if (rb[0][0] == R and rb[0][1] == C) or (rb[1][0] == R and rb[1][1] == C):
+            return sec
 
-        # 회전
-        if r[0][0] - r[1][0] == 0:
-            for d in range(4):
-                check_x = r[0][0] + rotate[d][0]
-                check_y = r[0][1] + rotate[d][1]
-                nx = r[0][0] + rotate[d][2]
-                ny = r[0][1] + rotate[d][3]
-                if not (0 <= check_x <= N and 0 <= check_y <= N and 0 <= nx <= N and 0 <= ny <= N): continue
-                if board[check_y][check_x] or board[ny][nx]: continue
-                if (abs(nx-r[1][0]) + abs(ny-r[1][1])) != 1: continue
-                v = str(nx) + str(ny) + str(r[1][0]) + str(r[1][1])
-                if visit[v] > 0: continue
-                q.append([[[nx, ny], [r[1][0], r[1][1]]], sec + 1])
-                visit[v] = 1
+        #90도 회전
+        for l in range(2):
+            for i in range(2):
+                r = rb[(l+1)%2][0]
+                c = rb[(l+1)%2][1]
+                nr1 = rb[l][0] + rotate[rb[2]][l][i][0][0]
+                nc1 = rb[l][1] + rotate[rb[2]][l][i][0][1]
+                nr2 = rb[l][0] + rotate[rb[2]][l][i][1][0]
+                nc2 = rb[l][1] + rotate[rb[2]][l][i][1][1]
+                if not (0 <= nr1 <= R and 0 <= nr2 <= R and 0 <= nc1 <= C and 0 <= nc2 <= C): continue
+                if brd[nr1][nc1] or brd[nr2][nc2]: continue
+                if rb[2] == 0:
+                    if r < nr2:
+                        r1, c1, r2, c2 = r, c, nr2, nc2
+                    else:
+                        r1, c1, r2, c2 = nr2, nc2, r, c
+                    if visit[1][r1][c1][r2][c2]: continue
+                    visit[1][r1][c1][r2][c2] = 1
+                    q.append([[[r1, c1], [r2, c2], 1], sec+1])
 
-            for d in range(4):
-                check_x = r[1][0] + rotate[d][0]
-                check_y = r[1][1] + rotate[d][1]
-                nx = r[1][0] + rotate[d][2]
-                ny = r[1][1] + rotate[d][3]
-                if not (0 <= check_x <= N and 0 <= check_y <= N and 0 <= nx <= N and 0 <= ny <= N): continue
-                if board[check_y][check_x] or board[ny][nx]: continue
-                if (abs(nx-r[0][0]) + abs(ny-r[0][1])) != 1: continue
-                v = str(r[0][0]) + str(r[0][1]) + str(nx) + str(ny)
-                if visit[v] > 0: continue
-                q.append([[[r[0][0], r[0][1]], [nx, ny]], sec + 1])
-                visit[v] = 1
+                else:
+                    if c < nc2:
+                        r1, c1, r2, c2 = r, c, nr2, nc2
+                    else:
+                        r1, c1, r2, c2 = nr2, nc2, r, c
+                    if visit[0][r1][c1][r2][c2]: continue
+                    visit[0][r1][c1][r2][c2] = 1
+                    q.append([[[r1, c1], [r2, c2], 0], sec+1])
 
-        else:
-            for d in range(4, 8):
-                check_x = r[0][0] + rotate[d][0]
-                check_y = r[0][1] + rotate[d][1]
-                nx = r[0][0] + rotate[d][2]
-                ny = r[0][1] + rotate[d][3]
-                if not (0 <= check_x <= N and 0 <= check_y <= N and 0 <= nx <= N and 0 <= ny <= N): continue
-                if board[check_y][check_x] or board[ny][nx]: continue
-                if (abs(nx - r[1][0]) + abs(ny - r[1][1])) != 1: continue
-                v = str(nx) + str(ny) + str(r[1][0]) + str(r[1][1])
-                if visit[v] > 0: continue
-                q.append([[[nx, ny], [r[1][0], r[1][1]]], sec + 1])
-                visit[v] = 1
-
-            for d in range(4, 8):
-                check_x = r[1][0] + rotate[d][0]
-                check_y = r[1][1] + rotate[d][1]
-                nx = r[1][0] + rotate[d][2]
-                ny = r[1][1] + rotate[d][3]
-                if not (0 <= check_x <= N and 0 <= check_y <= N and 0 <= nx <= N and 0 <= ny <= N): continue
-                if board[check_y][check_x] or board[ny][nx]: continue
-                if (abs(nx - r[0][0]) + abs(ny - r[0][1])) != 1: continue
-                v = str(r[0][0]) + str(r[0][1]) + str(nx) + str(ny)
-                if visit[v] > 0: continue
-                q.append([[[r[0][0], r[0][1]], [nx, ny]], sec + 1])
-                visit[v] = 1
-
-        # 이동
-        for m in range(4):
-            rx1 = r[0][0] + move[m][0]
-            ry1 = r[0][1] + move[m][1]
-            rx2 = r[1][0] + move[m][0]
-            ry2 = r[1][1] + move[m][1]
-            if not (0 <= rx1 <= N and 0 <= ry1 <= N and 0 <= rx2 <= N and 0 <= ry2 <= N): continue
-            if board[ry1][rx1] or board[ry2][rx2]: continue
-            v = str(rx1) + str(ry1) + str(rx2) + str(ry2)
-            if visit[v] > 0: continue
-            visit[v] = 1
-            q.append([[[rx1, ry1], [rx2, ry2]], sec + 1])
-
+        #한 칸 이동
+        for ddr, ddc in zip(dr, dc):
+            r1 = rb[0][0] + ddr
+            c1 = rb[0][1] + ddc
+            r2 = rb[1][0] + ddr
+            c2 = rb[1][1] + ddc
+            if not (0 <= r1 <= R and 0 <= r2 <= R and 0 <= c1 <= C and 0 <= c2 <= C): continue
+            if brd[r1][c1] or brd[r2][c2]: continue
+            if visit[rb[2]][r1][c1][r2][c2]: continue
+            visit[rb[2]][r1][c1][r2][c2] = 1
+            q.append([[[r1, c1], [r2, c2], rb[2]], sec+1])
 
 def solution(board):
-    return play(board, len(board) - 1)
+    global brd, rotate
+    brd = board
+    rotate = [
+        #d = 0일 때,
+        [[[[1, 0], [1, 1]], [[-1, 0], [-1, 1]]], [[[1, 0], [1, -1]], [[-1, 0], [-1, -1]]]],
+        #d = 1일 때,
+        [[[[0, 1], [1, 1]], [[0, -1], [1, -1]]], [[[0, 1], [-1, 1]], [[0, -1], [-1, -1]]]]
+    ]
+
+    return  bfs(len(board)-1, len(board[0])-1)
